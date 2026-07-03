6afbd6dfe210cbea8fa325c4b852131cd928233d
Marco Antonio Castro Cervantes <marcocastroce03@gmail.com>
2026-07-03 11:27:22 -0600

    Se agrego consultacontacto.html y cambios en views

--- prueba/registros/views.py
+++ prueba/registros/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Alumnos
+from .models import Alumnos, ComentarioContacto 
 from .forms import ComentarioContactoForm
 
 # Create your views here.
@@ -10,12 +10,17 @@ def registros (request):
 def registrar(request):
     if request.method == 'POST':
         form = ComentarioContactoForm(request.POST)
-        if form.is_valid():#si los datos recibidos son correctos
-            form.save()#inserta
-            return render(request,'registros/contacto.html')
+        if form.is_valid(): 
+            form.save()
+            comentarios = ComentarioContacto.objects.all()
+            return render(request, "registros/consultaContacto.html", {'comentarios': comentarios})
     form = ComentarioContactoForm()
     #si saale mal se va a reenviar al formulario los datos ingresados
-    return render(request,'registros/contacto.html',{'form':form})    
+    return render(request,'registros/contacto.html',{'form':form})   
 
 def contacto(request):
     return render(request,"registros/contacto.html")
+
+def consultarComentario(request):
+    comentarios = ComentarioContacto.objects.all()
+    return render(request, "registros/consultaContacto.html", {'comentarios': comentarios})
